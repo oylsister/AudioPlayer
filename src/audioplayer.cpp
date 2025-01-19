61bf37ca0f0ec7ecdfada3ccfca1fdb6a876faba
Oylsister <kkuiba2009@gmail.com>
2025-01-19 21:18:56 +0700

    Make it sympho name of course

--- src/audioplayer.cpp
+++ src/audioplayer.cpp
@@ -119,6 +119,8 @@ void SendVoiceDataLoop()
             }
         }
 
+        auto audioPlayer = GetFakeClient("Sympho")->GetPlayerSlot().Get();
+
         for (int i = 0; i < client_list->Count(); i++)
         {
             if (!client_list->IsValidIndex(i))
@@ -170,7 +172,7 @@ void SendVoiceDataLoop()
             // 1 (non-exist but legit client index) -> a skeleton icon with no name playing the audio, no need sv_alltalk 1
             // 1337 (non-exist and illegal client index) -> no display, but still playing audio, no need sv_alltalk 1
             // btw, calling CreateFakeClient in this thread will cause weird bug in counterstrikesharp
-            pData->set_client(-1);
+            pData->set_client(audioPlayer);
             client->GetNetChannel()->SendNetMessage(pData, NetChannelBufType_t::BUF_VOICE);
             if (player_data.msg)
             {
